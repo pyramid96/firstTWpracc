6e4631ecdb9ce5554c3204bf21daa0c15bd4dfc6
Noel Fődi <70132738+pyramid96@users.noreply.github.com>
2023-04-03 11:28:56 +0200

    Adding the first few lines

---
+++ pracc.js
@@ -0,0 +1,99 @@
+// Defining ourtTeam object
+
+let ourTeam ={
+    "name": "randomName",
+    "members": [
+        {
+            "name": "Fődi Noel",
+            "birthday": {
+                "year": 1996,
+                "month": 2,
+                "day": 25
+            },
+            "location": {
+                "country": "Hungary",
+                "city": "Szeged"
+            },
+            "coding level": 12
+        },
+        {
+            "name": "Babos Bence",
+            "birthday": {
+                "year": 1994,
+                "month": 6,
+                "day": 23
+            },
+            "location": {
+                "country": "Hungary",
+                "city": "Elfelejtettem"
+            },
+            "coding level": 22
+        },
+        {
+            "name": "Dániel József Kiss",
+            "birthday": {
+                "year": 1997,
+                "month": 4,
+                "day": 28
+            },
+            "location": {
+                "country": "Hungary",
+                "city": "Budapest"
+            },
+            "coding level": 19
+        },
+        {
+            "name": "Dávid Szokolóczi",
+            "birthday": {
+                "year": 2001,
+                "month": 6,
+                "day": 29
+            },
+            "location": {
+                "country": "Hungary",
+                "city": "Szeged"
+            },
+            "coding level": 28
+        }
+
+    ]
+
+
+};
+console.log(ourTeam["members"][1]["coding level"])
+
+
+// Adding the 'favourites' key to the member objects with 2 keys with the array values
+ourTeam["members"][0]["favourites"] = {
+    "favMovies": ["Fear and Loathing in Las Vegas", "Trainspotting", "Life of Brian", "The Big Lebowski"],
+    "favBooks": ["Dune", "The Silence of the Lambs"]
+};
+
+ourTeam["members"][1]["favourites"] = {
+    "favMovies": ["Fear and Loathing in Las Vegas", "Trainspotting", "Life of Brian", "The Big Lebowski"],
+    "favBooks": ["Dune", "The Silence of the Lambs"]
+};
+
+ourTeam["members"][2]["favourites"] = {
+    "favMovies": ["Fear and Loathing in Las Vegas", "Trainspotting", "Life of Brian", "The Big Lebowski"],
+    "favBooks": ["Dune", "The Silence of the Lambs"]
+};
+ourTeam["members"][3]["favourites"] = {
+    "favMovies": ["Fear and Loathing in Las Vegas", "Trainspotting", "Life of Brian", "The Big Lebowski"],
+    "favBooks": ["Dune", "The Silence of the Lambs"]
+};
+
+
+
+
+console.log(ourTeam["members"][0]["favourites"]["favBooks"])
+
+// Configuring the coding level key key
+ourTeam["members"][0]["coding level"] = {
+    "level": 12,
+    "seniority": ""
+}
+if (ourTeam["members"][0]["coding level"] < 50){
+    ourTeam["members"][0]["coding level"]["seniority"] = "junior"
+}
+else if (ourTeam["members"][0]["coding level"] >= 50 && )
