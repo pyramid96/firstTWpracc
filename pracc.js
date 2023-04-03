35aa36f9fcb574c774b11d5a3d4e0ae98c010f0b
davidszoki <davidszokoloczi@gmail.com>
2023-04-03 15:10:50 +0200

    add task to the file

--- pracc.js
+++ pracc.js
@@ -57,43 +57,12 @@ let ourTeam ={
         }
 
     ]
-
-
-};
-console.log(ourTeam["members"][1]["coding level"])
-
-
-// Adding the 'favourites' key to the member objects with 2 keys with the array values
-ourTeam["members"][0]["favourites"] = {
-    "favMovies": ["Fear and Loathing in Las Vegas", "Trainspotting", "Life of Brian", "The Big Lebowski"],
-    "favBooks": ["Dune", "The Silence of the Lambs"]
 };
 
-ourTeam["members"][1]["favourites"] = {
-    "favMovies": ["Fear and Loathing in Las Vegas", "Trainspotting", "Life of Brian", "The Big Lebowski"],
-    "favBooks": ["Dune", "The Silence of the Lambs"]
-};
-
-ourTeam["members"][2]["favourites"] = {
-    "favMovies": ["Fear and Loathing in Las Vegas", "Trainspotting", "Life of Brian", "The Big Lebowski"],
-    "favBooks": ["Dune", "The Silence of the Lambs"]
-};
-ourTeam["members"][3]["favourites"] = {
-    "favMovies": ["Fear and Loathing in Las Vegas", "Trainspotting", "Life of Brian", "The Big Lebowski"],
-    "favBooks": ["Dune", "The Silence of the Lambs"]
-};
+//----------------The youngest member----------------
 
 
+//----------------The oldest member----------------
 
 
-console.log(ourTeam["members"][0]["favourites"]["favBooks"])
-
-// Configuring the coding level key key
-ourTeam["members"][0]["coding level"] = {
-    "level": 12,
-    "seniority": ""
-}
-if (ourTeam["members"][0]["coding level"] < 50){
-    ourTeam["members"][0]["coding level"]["seniority"] = "junior"
-}
-else if (ourTeam["members"][0]["coding level"] >= 50 && )
+//----------------From the same location?----------------
