5cb6661a72afd415d068c508cccf01d515130461
davidszoki <davidszokoloczi@gmail.com>
2023-04-03 15:40:15 +0200

    clean data, and first task

--- pracc.js
+++ pracc.js
@@ -1,66 +1,85 @@
 // Defining ourtTeam object
 
-let ourTeam ={
-    "name": "randomName",
-    "members": [
-        {
-            "name": "Fődi Noel",
-            "birthday": {
-                "year": 1996,
-                "month": 2,
-                "day": 25
+let ourTeam = 
+[
+    {
+        name: "randomName",
+        members:
+        [
+            {
+                "name": "Fődi Noel",
+                "birthday": 
+                    {
+                        year: 1996,
+                        "month": 2,
+                        "day": 25
+                    },
+                "location":
+                    {
+                        "country": "Hungary",
+                        "city": "Szeged"
+                    },
+                "coding level": 12
             },
-            "location": {
-                "country": "Hungary",
-                "city": "Szeged"
+            {
+                "name": "Norbert Richter",
+                "birthday": 
+                    {
+                        year: 1994,
+                        "month": 12,
+                        "day": 27
+                    },
+                "location": 
+                    {
+                        "country": "Hungary",
+                        "city": "Szentendre"
+                    },
+                "coding level": 15
             },
-            "coding level": 12
-        },
-        {
-            "name": "Norbert Richter",
-            "birthday": {
-                "year": 1994,
-                "month": 12,
-                "day": 27
+            {
+                "name": "Orosz Péter",
+                "birthday": 
+                    {
+                        year: 1988,
+                        "month": 5,
+                        "day": 9
+                    },
+                "location": 
+                    {
+                        "country": "Hungary",
+                        "city": "Érd"
+                    },
+                "coding level": 20
             },
-            "location": {
-                "country": "Hungary",
-                "city": "Szentendre"
-            },
-            "coding level": 15
-        },
-        {
-            "name": "Orosz Péter",
-            "birthday": {
-                "year": 1988,
-                "month": 5,
-                "day": 9
-            },
-            "location": {
-                "country": "Hungary",
-                "city": "Érd"
-            },
-            "coding level": 20
-        },
-        {
-            "name": "Dávid Szokolóczi",
-            "birthday": {
-                "year": 2000,
-                "month": 10,
-                "day": 21
-            },
-            "location": {
-                "country": "Hungary",
-                "city": "Balassagyarmat"
-            },
-            "coding level": 28
-        }
+            {
+                "name": "Dávid Szokolóczi",
+                "birthday": 
+                    {
+                        year: 2000,
+                        "month": 10,
+                        "day": 21
+                    },
+                "location": 
+                    {
+                        "country": "Hungary",
+                        "city": "Balassagyarmat"
+                    },
+                "coding level": 28
+            }
 
-    ]
-};
+        ]
+    }
+]
 
 //----------------The youngest member----------------
+const youngestMember = ourTeam.members.birthday[0];
 
+for(const member of ourTeam.members.birthday){
+    if (member.year > youngestMember.year) {
+        youngestMember = member;
+    }
+}
+console.log(youngestMember);
 
 //----------------The oldest member----------------
 
