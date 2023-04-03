896d60e152b5344fbbade29a0b5ab7f73294ba73
Noel Fődi <70132738+pyramid96@users.noreply.github.com>
2023-04-03 15:13:24 +0200

    adding my music

--- pracc.js
+++ pracc.js
@@ -1,4 +1,142 @@
+<<<<<<< Updated upstream
 // Defining ourtTeam object
+=======
+let noelFavBooks = [
+    {
+        "title": "Dune",
+        "author": "Frank Herbert",
+        "year": 1965,
+        "isNewerThan2000": false,
+        "age": 58,
+        "characters": ["Paul Atreides", "Lady Jessica", "Stilgar", "Vladimir Harkonnen"]
+    
+    },
+    {
+        "title": "The Silence of the Lambs",
+        "author": "Thomas Harris",
+        "year": 1988,
+        "isNewerThan2000": false,
+        "age": 35,
+        "characters": ["Clarice Starling", "Hannibal Lecter", "Jack Crawford", "Buffalo Bill"]
+    }];
+    
+    
+    let noelFavMovies = [{
+        "title": "Fear and Loathing in Las Vegas",
+        "year": 1998,
+        "rating": 7.5,
+        "description":"Raoul Duke and Dr. Gonzo travel to Las Vegas for a series of psychedelic escapades.",
+        "directors": ["Terry Gilliam", "Terry Gilliam"],
+        "writers": ["Hunter S. Thompson", "Terry Gilliam", "Tony Grisoni"],
+        "stars": ["Johnny Depp", "Benicio del Toro", "Tobey Maguire"],
+        "genres": ["adventure", "comedy", "drama"]
+    },
+    
+    
+    {
+        "title": "The Big Lebowski",
+        "year": 1998,
+        "rating": 8.1,
+        "description":"The 'Dude' Lebowski, mistaken for a millionaire of the same name, " +
+        "seeks restitution for a rug ruined by debt collectors, enlisting his bowling buddies for help while trying to find the millionaire's missing wife.",
+        "directors": ["Joel Coen", "Ethan Coen"],
+        "writers": ["Joel Coen", "Ethan Coen"],
+        "stars": ["Jeff Bridges", "John Goodman", "Julianne Moore"],
+        "genres": ["comedy", "crime"]
+    
+    },
+    
+    {
+        "title": "Life of Brian",
+        "year": 1979,
+        "rating": 8.0,
+        "description":"Born on the original Christmas in the stable next door to Jesus Christ, " +
+        "Brian of Nazareth spends his life being mistaken for a messiah.",
+        "directors": ["Terry Jones", "Terry Jones"],
+        "writers":["Graham Chapman", "John Cleese", "Terry Gilliam"],
+        "stars": ["Graham Chapman", "John Cleese", "Michael Palin"],
+        "genres": ["comedy", "drama"]
+    },
+    
+    {
+        "title": "Trainspotting",
+        "year": 1996,
+        "rating": 8.1,
+        "description":"Renton, deeply immersed in the Edinburgh drug scene, tries to clean up and get out, " +
+        "despite the allure of the drugs and influence of friends.",
+        "directors": ["Danny Boyle", "Danny Boyle"],
+        "writers":["Irvine Welsh", "John Hodge"],
+        "stars": ["Ewan McGregor", "Ewen Brenmer", "Jonny Lee Miller"],
+        "genres": ["drama", "comedy"]
+    }];
+
+//Norbi:
+let favoriteBooks = [
+	{
+		title: "The Lord of the Rings: The Two Towers",
+		author: "J.R.R. Tolkien",
+		year: 1955,
+		isNewerThan2000: false,
+		characters: ["Gimli", "Gandalf", "Aragorn", "Sam", "Frodo"],
+	},
+	{
+		title: "1984",
+		author: "George Orwell",
+		year: 1949,
+		isNewerThan2000: false,
+		characters: ["WinstonSmith", "Julia", "OBrien", "Syme"],
+	},
+];
+
+let NorbifavMovies =[
+	{
+		title: "Pulp Fiction",
+		year: 1994,
+		age: 2023 - 1994,
+		rating: 8.9,
+		description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+		stars: ["John Travolta", "Uma Thurman", "Samuel L. Jackson", "Bruce Willis", "Tim Roth"],
+		writers: ["Quenting Tarantino", "Roger Avary"],
+		directors: ["Quentin Tarantino"],
+		genres: ["Crime, Drama"],
+	},
+	{
+		title: "Reservoir Dogs",
+		year: 1992,
+		age: 2023 - 1992,
+		rating: 8.3,
+		description: "When a simple jewelry heist goes horribly wrong, the surviving criminals begin to suspect that one of them is a police informant.",
+		stars: ["Harvey Keitel", "Tim Roth", "Michael Madsen"],
+		writers: ["Quentin Tarantino", "Roger Avary"],
+		directors: ["Quentin Tarantino"],
+		genres: ["Crime", "Thriller"],
+	},
+	{
+		title: "The Lord of the Rings",
+		year: 2001,
+		age: 2023 - 2001,
+		rating: 8.8,
+		description: "Set in the fictional world of Middle-earth, the films follow the hobbit Frodo Baggins as he and the Fellowship embark on a quest to destroy the One Ring, to ensure the destruction of its maker, the Dark Lord Sauron.",
+		stars: ["Elijah Wood", "Ian McKellen", "Orlando Bloom"],
+		writers: ["J.R.R. Tolkien", "Fran Walsh", "Philippa Boyens"],
+		directors: ["Peter Jackson"],
+		genres: ["Action", "Adventure", "Drama"],
+	},
+	{
+		title: "Chernobyl",
+		year: 2019,
+		age: 2023 - 2019,
+		rating: 9.4,
+		description: "Chernobyl is a 2019 historical drama television miniseries that revolves around the Chernobyl disaster of 1986 and the cleanup efforts that followed.",
+		stars: ["Jessie Buckley", "Jared Harris", "Stellan Skarsgård"],
+		writers: ["Craig Mazin"],
+		directors: ["Johan Renck"],
+		genres: ["Drama", "History", "Thriller"],
+	},
+];
+
+
+>>>>>>> Stashed changes
 
 let ourTeam ={
     "name": "randomName",
